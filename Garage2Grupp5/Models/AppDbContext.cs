87fb9305680361588d87e1086d5c50e5f09e979e
Attila Starkenius <symphtom@hotmail.com>
2022-12-27 14:23:48 +0100

    27.12.2022. Attila Starkenius adding AddScoped to Program.cs file

--- Garage2Grupp5/Models/AppDbContext.cs
+++ Garage2Grupp5/Models/AppDbContext.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Threading.Tasks;
+using Garage2Grupp5.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Garage2Grupp5.Models
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ParkedVehicle> ParkedVehicles { get; set; }
     }
 }
--- Garage2Grupp5/Program.cs
+++ Garage2Grupp5/Program.cs
@@ -2,15 +2,22 @@
 using Microsoft.Extensions.DependencyInjection;
 using Garage2Grupp5.Data;
 using System;
+using Garage2Grupp5.Models;
 //var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<Garage2Grupp5Context>(options =>
 //    options.UseSqlServer(builder.Configuration.GetConnectionString("Garage2Grupp5Context") ?? throw new InvalidOperationException("Connection string 'Garage2Grupp5Context' not found.")));
+
+var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IParkedVehicleRepository, ParkedVehicleRepository>();
+
 
 var app = builder.Build();
 
