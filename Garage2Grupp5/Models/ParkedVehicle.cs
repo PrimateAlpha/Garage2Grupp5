844c646a8cebf582e840ed4f56a642bf6b92816b
Attila Starkenius <symphtom@hotmail.com>
2022-12-27 11:05:27 +0100

    27.12.2022. Attila Starkenius bransch

---
+++ Garage2Grupp5/Models/ParkedVehicle.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage2Grupp5.Models
+{
+    public class ParkedVehicle
+    {
+        [Key] public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "LicensePlate")]
+        [StringLength(20)]
+        [Key] public string LicensePlate { get; set; }
+
+        public string Type { get; set; }
+        [Display(Name = "Type")]
+        [Range(5, 100)]
+        public int Type { get; set; }
+        public string Department { get; set; }
+    }
+}
