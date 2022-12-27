b21d8be84b44252edaf986dc75a1dba77eb56616
Attila Starkenius <symphtom@hotmail.com>
2022-12-27 12:13:38 +0100

    27.12.2022. Attila Starkenius adding AppDBContext.cs

---
+++ Garage2Grupp5/Models/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Garage2Grupp5.Models
+{
+    public class AppDbContext: DbContext
+    {
+    }
+}
--- Garage2Grupp5/Program.cs
+++ Garage2Grupp5/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Garage2Grupp5.Data;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<Garage2Grupp5Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Garage2Grupp5Context") ?? throw new InvalidOperationException("Connection string 'Garage2Grupp5Context' not found.")));
+using System;
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddDbContext<Garage2Grupp5Context>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("Garage2Grupp5Context") ?? throw new InvalidOperationException("Connection string 'Garage2Grupp5Context' not found.")));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
