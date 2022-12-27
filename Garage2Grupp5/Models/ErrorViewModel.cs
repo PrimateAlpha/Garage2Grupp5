01e89dea653cee7289e68c7bf2c79fb2bf500e05
Christian Norman <christian.norman@hotmail.se>
2022-12-27 10:12:46 +0100

    Add project files.

---
+++ Garage2Grupp5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Garage2Grupp5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
