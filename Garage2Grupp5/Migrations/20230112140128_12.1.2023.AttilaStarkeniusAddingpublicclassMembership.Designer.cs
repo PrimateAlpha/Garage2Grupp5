894371ff723c52c45346809b01fac35870ba989c
Attila Starkenius <symphtom@hotmail.com>
2023-01-12 15:02:14 +0100

    12.1.2023. Attila Starkenius Adding public class Membership

--- Garage2Grupp5/Migrations/20230112140128_12.1.2023.AttilaStarkeniusAddingpublicclassMembership.Designer.cs
+++ Garage2Grupp5/Migrations/20230112140128_12.1.2023.AttilaStarkeniusAddingpublicclassMembership.Designer.cs
@@ -4,6 +4,7 @@ using Garage2Grupp5.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Garage2Grupp5.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230112140128_12.1.2023.AttilaStarkeniusAddingpublicclassMembership")]
+    partial class _1212023AttilaStarkeniusAddingpublicclassMembership
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Garage2Grupp5/Models/Membership.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage2Grupp5.Models
+{
+    public class Membership
+    {
+        [Key] public int Id { get; set; }
+
+        //Navigation properties        
+        public ICollection<ParkedVehicle> ParkedVehicles { get; set; }
+        //public ICollection<Enrollment> Enrollments { get; set; }
+    
+    public string SocialSecurityNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+
+
+    }
+}
