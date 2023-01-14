68a58976f5b677d52c325be82af1e59b19b4fb5b
Attila Starkenius <symphtom@hotmail.com>
2023-01-14 09:52:14 +0100

    Attila Starkenius 14.1.2023. Adding ParkedVehicle.cshtml

--- Garage2Grupp5/Models/VehicleType.cs
+++ Garage2Grupp5/Models/VehicleType.cs
@@ -4,6 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        //Navigation properties        
+
+        public ICollection<ParkedVehicle> ParkedVehicles { get; set; }
+
         //Car,
         //Buss,
         //Boat
---
+++ Garage2Grupp5/ViewModels/ParkedVehicleViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Garage2Grupp5.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Garage2Grupp5.ViewModels
+{
+    public class ParkedVehicleViewModel
+    {
+        [Key] public int Id { get; set; }
+
+        public int MemberId { get; set; }
+
+        [Required]
+        [Display(Name = "LicensePlate")]
+        [StringLength(20)]
+        //Try disable javascript in microsoft edge while debugging this Remote:
+        //like this in microsoft edge:
+        //Settings -> Preferences ->
+        //[Remote("IsLicensePlateExist", "ParkedVehicle", AdditionalFields = "Id",
+        //        ErrorMessage = "LicensePlate already exists")]
+        public string LicensePlate { get; set; } = string.Empty;
+
+        public VehicleType? Type { get; set; }
+        //public VehicleType Type { get; set; }/* = VehicleType.;*/
+        public string VehicleType { get; set; } /*= string.Empty;*/
+
+
+
+        [Display(Name = "Nr of wheels")]
+        [Range(0, 20)]
+        //public int Type { get; set; }
+        public int NrOfWheels { get; set; }
+
+        public string Color { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public DateTime ArrivalTime { get; set; } = DateTime.Parse(DateTime.Now.ToString("g"));
+
+        public DateTime DepartureTime { get; set; } = DateTime.Parse(DateTime.Now.ToString("g"));
+
+        public decimal Price { get; set; }
+    }
+}
