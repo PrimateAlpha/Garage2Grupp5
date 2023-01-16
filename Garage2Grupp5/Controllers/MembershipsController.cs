3ddf716e3d2ea6c21d041ca5a0ffc7dd03164aa5
Attila Starkenius <symphtom@hotmail.com>
2023-01-16 15:40:04 +0100

    16.1.2023. Attila Starkenius MemberShipController Create Method just commented out not working

--- Garage2Grupp5/Controllers/MembershipsController.cs
+++ Garage2Grupp5/Controllers/MembershipsController.cs
@@ -56,17 +56,17 @@ namespace Garage2Grupp5.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,SocialSecurityNumber,FirstName,LastName,FullName")] Membership membership)
         {
-            var vehicleType = await _context.VehicleType.FirstOrDefaultAsync(vt => vt.Name = registeredVehicle.VehicleType);
+            //var vehicleType = await _context.VehicleType.FirstOrDefaultAsync(vt => vt.Name = registeredVehicle.VehicleType);
 
-            var newVehicle = new /*RegisteredVehicleViewModel*/ParkedVehicle
-            {
-                LicensePlate = registeredVehicle.LicensePlate,
-                Brand = registeredVehicle.Brand,
-                VehicleTypeId = vehicleType.Id
-                //fyll på med resten av properties
-                //Type = vehicleType
-                //LicensePlate = vehicleType.
-            };
+            //var newVehicle = new /*RegisteredVehicleViewModel*/ParkedVehicle
+            //{
+            //    LicensePlate = registeredVehicle.LicensePlate,
+            //    Brand = registeredVehicle.Brand,
+            //    VehicleTypeId = vehicleType.Id
+            //    //fyll på med resten av properties
+            //    //Type = vehicleType
+            //    //LicensePlate = vehicleType.
+            //};
 
             if (ModelState.IsValid)
             {
